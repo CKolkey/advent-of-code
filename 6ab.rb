5ee93740d1bc7d2acbd283125fe7b81ea4e858d3
Cameron Kolkey <Alleyria@gmail.com>
2020-12-07 13:05:17 +0000

    day 6

---
+++ 6ab.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# Part 1
+puts File.read("6.input")
+         .split("\n\n")
+         .map { |a| a.scan(/\S/).uniq.length }
+         .sum
+
+
+# part 2 (2947)
+p File.read("6.input")
+      .split("\n\n")
+      .map(&:split)
+      .map { |arr| arr.map { |a| a.scan(/\S/) }.inject(:&).length }
+      .sum
