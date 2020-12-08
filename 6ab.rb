a854caa7ae35081b9954ac7ae06e897fde9af2cb
Cameron Kolkey <Alleyria@gmail.com>
2020-12-08 10:14:49 +0000

    rubocop and refactor 6ab

--- 6ab.rb
+++ 6ab.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
+INPUT = File.read("6.input").split("\n\n")
+
 # Part 1
-p File.read("6.input")
-      .split("\n\n").sum { |a| a.scan(/\S/).uniq.length }
+p(INPUT.sum { |a| a.scan(/\S/).uniq.length })
 
 # part 2 (2947)
-p File.read("6.input")
-      .split("\n\n")
-      .map(&:split).sum { |arr| arr.map { |a| a.scan(/\S/) }.inject(:&).length }
+p(INPUT.map(&:split).sum { |arr| arr.map { |a| a.scan(/\S/) }.inject(:&).length })
