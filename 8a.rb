1f4ae8b5990ffc380b56e84e22fb17d79d682d16
Cameron Kolkey <Alleyria@gmail.com>
2020-12-08 09:03:28 +0000

    add day 8a

---
+++ 8a.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+INSTRUCTIONS = File.readlines("8.input").map(&:split).map { |(inst, n)| [inst, n.to_i] }
+accumulator  = 0
+offset       = 0
+execution    = {}
+
+loop do
+  inst, n = INSTRUCTIONS[offset]
+
+  case inst
+  when "acc"
+    accumulator += n
+    offset += 1
+  when "jmp"
+    offset += n
+  when "nop"
+    offset += 1
+  end
+
+  break if execution[offset]
+  execution[offset] = [inst, n]
+end
+
+puts accumulator
