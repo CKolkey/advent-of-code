25ff0c18301d3a3b3b19b77b01db6c3455c2f838
Cameron Kolkey <Alleyria@gmail.com>
2020-12-08 10:05:43 +0000

    rubocop 6a/b

--- 6ab.rb
+++ 6ab.rb
@@ -2,14 +2,9 @@
 
 # Part 1
 p File.read("6.input")
-      .split("\n\n")
-      .map { |a| a.scan(/\S/).uniq.length }
-      .sum
-
+      .split("\n\n").sum { |a| a.scan(/\S/).uniq.length }
 
 # part 2 (2947)
 p File.read("6.input")
       .split("\n\n")
-      .map(&:split)
-      .map { |arr| arr.map { |a| a.scan(/\S/) }.inject(:&).length }
-      .sum
+      .map(&:split).sum { |arr| arr.map { |a| a.scan(/\S/) }.inject(:&).length }
