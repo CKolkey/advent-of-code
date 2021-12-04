e027df46a5623bbe2ede4abe58a854adc2116dd5
Cameron <Alleyria@gmail.com>
2021-12-04 10:40:42 +0100

    better input handling for day 4

--- 4ab.rb
+++ 4ab.rb
@@ -64,28 +64,22 @@ class Square
   end
 end
 
-NUMBERS = [
-  42, 32, 13, 22, 91, 2, 88, 85, 53, 87, 37, 33, 76, 98, 89, 19, 69, 9, 62, 21, 38, 49,
-  54, 81, 0, 26, 79, 36, 57, 18, 4, 40, 31, 80, 24, 64, 77, 97, 70, 6, 73, 23, 20, 47,
-  45, 51, 74, 25, 95, 96, 58, 92, 94, 11, 39, 63, 65, 99, 48, 83, 29, 34, 44, 75, 55, 17,
-  14, 56, 8, 82, 59, 52, 46, 90, 5, 41, 60, 67, 16, 1, 15, 61, 71, 66, 72, 30, 28, 3, 43,
-  27, 78, 10, 86, 7, 50, 35, 84, 12, 93, 68
-].freeze
+input   = File.read('4.input').split("\n\n")
+numbers = input.first.split(',').map(&:to_i)
+boards  = input.slice(1..).map { |board| Board.new(board) }
 
-boards = File.read('4.input').split("\n\n").map { |board| Board.new(board) }
 first  = nil
 last   = nil
 
-NUMBERS.each do |number|
+numbers.each do |number|
   boards.each do |board|
     next if board.winner?
 
     board.mark(number)
     next unless board.winner?
 
-    score = board.compute_final_score
+    score = last = board.compute_final_score
     first ||= score
-    last = score
   end
 end
 
