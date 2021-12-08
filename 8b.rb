ea2343be31e3e431c2118137e8cac8bf16c9956c
Cameron <Alleyria@gmail.com>
2021-12-08 17:33:53 +0100

    SIMPLIFY!

--- 8b.rb
+++ 8b.rb
@@ -1,27 +1,31 @@
 # frozen_string_literal: true
 
 require "debug"
+require "set"
 
 input = File.read("8.input")
-test_input = File.read("8.test.input")
 
 class Display
   attr_reader :numbers
 
   def initialize(signal, value)
-    @signal  = signal.map { |word| word.chars.sort.join }
-    @value   = value.map { |word| word.chars.sort.join }
+    @signal  = signal.map { |word| word.chars.to_set }
+    @value   = value.map { |word| word.chars.to_set }
     @numbers = {}
   end
 
   def call
-    map_easy
-    map_three
-    map_zero
-    map_nine
-    map_six
-    map_five
-    map_two
+    @numbers[1] = find_by_length(2)
+    @numbers[7] = find_by_length(3)
+    @numbers[4] = find_by_length(4)
+    @numbers[8] = find_by_length(7)
+    @numbers[3] = find_by_length(5) { |n| numbers[7].subset?(n) }
+    @numbers[0] = find_by_length(6) { |n| !((numbers[4] - numbers[1]) & numbers[3]).subset?(n) }
+    @numbers[9] = find_by_length(6) { |n| numbers.values.none? { _1 == n } && numbers[3].subset?(n) }
+    @numbers[6] = find_by_length(6) { |n| numbers.values.none? { _1 == n } }
+    @numbers[5] = numbers[9] - (numbers[4] - numbers[6])
+    @numbers[2] = @signal.find { |n| numbers.values.none? { _1 == n } }
+
     self
   end
 
@@ -31,67 +35,13 @@ class Display
 
   private
 
-  def map_easy
-    @signal.each do |input|
-      case input.length
-      when 2
-        @numbers[1] = input
-      when 3
-        @numbers[7] = input
-      when 4
-        @numbers[4] = input
-      when 7
-        @numbers[8] = input
-      end
+  def find_by_length(length, &block)
+    if block.nil?
+      @signal.find { _1.length == length }
+    else
+      @signal.select { _1.length == length }.find(&block)
     end
-  end
-
-  def map_three
-    @numbers[3] = @signal.select { |n| n.length == 5 && numbers[7].chars.all? { |c| n.include?(c) } }.first
-  end
-
-  def map_zero
-    tmp = @signal.select { |n| n.length == 6 }
-    tmp = tmp.reject do |n|
-      n.include? numbers[4].chars
-                           .delete_if { numbers[1].include? _1 }
-                           .select { numbers[3].include? _1 }
-                           .first
-    end
-
-    @numbers[0] = tmp.first
-  end
-
-  def map_nine
-    @numbers[9] = @signal.select do |n|
-      n.length == 6 &&
-        !numbers.values.include?(n) &&
-        numbers[3].chars.all? { n.include?(_1) }
-    end.first
-  end
-
-  def map_six
-    @numbers[6] = @signal.select { |n| n.length == 6 && !numbers.values.include?(n) }.first
-  end
-
-  def map_five
-    missing = numbers[4].chars.reject { |c| numbers[6].include? c }.first
-    @numbers[5] = numbers[9].chars.reject { |c| c == missing }.sort.join
-  end
-
-  def map_two
-    @numbers[2] = @signal.select { |n| n.length == 5 && !numbers.values.include?(n) }.first
   end
 end
 
 puts(input.split("\n").map { Display.new(*_1.split(" | ").map(&:split)).call }.sum(&:output) == 1_097_568)
-
-# test_displays = test_input.split("\n").map { Display.new(*_1.split(" | ").map(&:split)).call }
-# test_outputs = [8394, 9781, 1197, 9361, 4873, 8418, 4548, 1625, 8717, 4315]
-
-# test_displays.each_with_index do |display, i|
-#   puts "Actual:   #{display.output}"
-#   puts "Expected: #{test_outputs[i]}"
-#   puts "Passing:  #{display.output == test_outputs[i]}"
-#   puts "----"
-# end
