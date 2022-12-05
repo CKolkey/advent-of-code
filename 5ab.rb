152d38cff1ab25588542798ab0417e5958e1dd9f
Cameron <Alleyria@gmail.com>
2022-12-05 20:49:14 +0100

    day 5

---
+++ 5ab.rb
@@ -0,0 +1,52 @@
+class CraneMover
+  attr_reader :stacks, :instructions
+
+  def initialize(stacks, instructions)
+    @stacks       = build_stacks stacks
+    @instructions = parse_instructions instructions
+  end
+
+  def results
+    stacks.values.map(&:first).join
+  end
+
+  def parse_instructions(instructions)
+    instructions.lines.map { _1.scan(/\d+/).map(&:to_i) }
+  end
+
+  def build_stacks(stacks)
+    stacks.lines[..-2]
+          .map { _1.scan(/(\w|\s{4})/).flatten }
+          .transpose
+          .map { _1.grep(/\w/) }
+          .map.with_index(1) { [_2, _1] }
+          .to_h
+  end
+end
+
+class CraneMover9000 < CraneMover
+  def perform!
+    instructions.each do |(quantity, from, to)|
+      quantity.times { stacks[to].unshift(stacks[from].shift) }
+    end
+  end
+end
+
+class CraneMover9001 < CraneMover
+  def perform!
+    instructions.each do |(quantity, from, to)|
+      stacks[to].unshift(stacks[from].shift(quantity))
+      stacks.transform_values!(&:flatten)
+    end
+  end
+end
+
+input = File.read("5.input").split("\n\n")
+
+cm9k = CraneMover9000.new(*input)
+cm9k.perform!
+puts cm9k.results # == "SPFMVDTZT"
+
+cm9k1 = CraneMover9001.new(*input)
+cm9k1.perform!
+puts cm9k1.results # == "ZFSJBPRFP"
