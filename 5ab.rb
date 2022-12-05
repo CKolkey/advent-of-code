36c182fbe4f362c603cffa402b4cd99f185d60e1
Cameron <Alleyria@gmail.com>
2022-12-05 21:01:37 +0100

    Polish

--- 5ab.rb
+++ 5ab.rb
@@ -6,9 +6,15 @@ class CraneMover
     @instructions = parse_instructions instructions
   end
 
+  def operate!
+    instructions.each { perform_instruction!(*_1) }
+  end
+
   def results
     stacks.values.map(&:first).join
   end
+
+  private
 
   def parse_instructions(instructions)
     instructions.lines.map { _1.scan(/\d+/).map(&:to_i) }
@@ -25,27 +31,23 @@ class CraneMover
 end
 
 class CraneMover9000 < CraneMover
-  def perform!
-    instructions.each do |(quantity, from, to)|
-      quantity.times { stacks[to].unshift(stacks[from].shift) }
-    end
+  def perform_instruction!(quantity, from, to)
+    quantity.times { stacks[to].unshift(stacks[from].shift) }
   end
 end
 
 class CraneMover9001 < CraneMover
-  def perform!
-    instructions.each do |(quantity, from, to)|
-      stacks[to].unshift(stacks[from].shift(quantity)).flatten!
-    end
+  def perform_instruction!(quantity, from, to)
+    stacks[to].unshift(stacks[from].shift(quantity)).flatten!
   end
 end
 
 input = File.read("5.input").split("\n\n")
 
 cm9k = CraneMover9000.new(*input)
-cm9k.perform!
+cm9k.operate!
 puts cm9k.results # == "SPFMVDTZT"
 
 cm9k1 = CraneMover9001.new(*input)
-cm9k1.perform!
+cm9k1.operate!
 puts cm9k1.results # == "ZFSJBPRFP"
