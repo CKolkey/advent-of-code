46b46bcd7ed179c694fb03f0f92ca25d9a30c1a4
Cameron <Alleyria@gmail.com>
2022-12-08 11:59:43 +0100

    day 7!

--- 7ab.rb
+++ 7ab.rb
@@ -1,40 +1,36 @@
-require "debug"
+def sum_tree(tree)
+  tree.except('..')
+      .values
+      .sum { |node| node.is_a?(Hash) ? sum_tree(node) : node }
+end
 
-input = <<~INPUT
-  $ cd /
-  $ ls
-  dir a
-  14848514 b.txt
-  8504156 c.dat
-  dir d
-  $ cd a
-  $ ls
-  dir e
-  29116 f
-  2557 g
-  62596 h.lst
-  $ cd e
-  $ ls
-  584 i
-  $ cd ..
-  $ cd ..
-  $ cd d
-  $ ls
-  4060174 j
-  8033020 d.log
-  5626152 d.ext
-  7214296 k
-INPUT
+def compute_tree_size!(tree)
+  tree.transform_values do |node|
+    next unless node.is_a? Hash
 
-input = File.read("7.input")
+    node[:size] = sum_tree(node)
+    compute_tree_size!(node.except('..'))
+  end
+end
+
+def collect_dirs(tree, collector = [])
+  tree.except('..').each_key do |name|
+    next unless tree[name].is_a?(Hash)
+
+    collector.push(tree[name][:size])
+    collect_dirs(tree[name], collector)
+  end
+
+  collector
+end
 
 cwd = filesystem = {}
-filesystem["/"] = { ".." => filesystem }
+filesystem['/'] = { '..' => filesystem }
 
-input.split("\n").each do |command|
+File.read('7.input').split("\n").each do |command|
   case command
   when /^dir/
-    cwd[command[4..]] = { ".." => cwd }
+    cwd[command[4..]] = { '..' => cwd }
   when /^\$ cd/
     cwd = cwd[command[5..]]
   when /^\d+/
@@ -43,48 +39,14 @@ input.split("\n").each do |command|
   end
 end
 
-def sum_tree(tree)
-  tree.except("..")
-      .values
-      .sum { |node| node.is_a?(Hash) ? sum_tree(node) : node }
-end
-
-def process_tree(tree)
-  tree.transform_values do |node|
-    next unless node.is_a? Hash
-
-    node[:size] = sum_tree(node)
-    process_tree(node.except(".."))
-  end
-end
-
-def collect_dirs(tree, collector = {})
-  tree.except("..").each_key do |name|
-    next unless tree[name].is_a?(Hash)
-
-    collector[name] = tree[name][:size]
-    collect_dirs(tree[name], collector)
-  end
-
-  collector
-end
-
-# puts sum_tree(filesystem.dig("/")) == 48_381_165
-# puts sum_tree(filesystem.dig("/", "a")) == 94_853
-# puts sum_tree(filesystem.dig("/", "a", "e")) == 584
-# puts sum_tree(filesystem.dig("/", "d")) == 24_933_642
-
-process_tree(filesystem)
-
-# puts filesystem.dig("/", :size) == 48_381_165
-# puts filesystem.dig("/", "a", :size) == 94_853
-# puts filesystem.dig("/", "a", "e", :size) == 584
-# puts filesystem.dig("/", "d", :size) == 24_933_642
-
+compute_tree_size!(filesystem)
 dirs = collect_dirs(filesystem)
 
-# puts dirs == { "/" => 48_381_165, "a" => 94_853, "e" => 584, "d" => 24_933_642 }
-# puts dirs.select { |_, v| v <= 100_000 }.values.sum == 95_437
-puts dirs.select { |_, v| v <= 100_000 }.values.sum
+# Part 1
+puts dirs.select { _1 <= 100_000 }.sum # == 1_783_610
 
-debugger
+# Part 2
+TOTAL    = 70_000_000
+MIN_FREE = 30_000_000
+FREE     = TOTAL - dirs[0]
+puts dirs.select { FREE + _1 > MIN_FREE }.min # == 4_370_655
