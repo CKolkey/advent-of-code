44c485ba4dac4f678db1b0f53378b22e02e93a84
Cameron <Alleyria@gmail.com>
2024-12-03 14:52:55 +0100

    Get cheeky with eval

--- 3.rb
+++ 3.rb
@@ -1,33 +1,31 @@
 # frozen_string_literal: true
 
+require "debug"
 input = File.read("3.input")
-
-def mul(a, b) = a * b
 
 # Part 1
 # input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 #
-puts input.scan(/mul\(\d+,\d+\)/).sum(&method(:eval))
+def mul(a, b) = a * b
+puts input.scan(/mul\(\d+,\d+\)/).sum(&method(:eval)) # 153_469_856
 
 # Part 2
 # input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 #
-instructions = input.scan(/(mul\(\d+,\d+\)|do\(\)|don't\(\))/).flatten
-skip = false
-sum = 0
-
-until instructions.empty?
-  case (instruction = instructions.shift)
-  when "don't()"
-    skip = true
-    next
-  when "do()"
-    skip = false
-    next
+instructions = input.scan(/(mul\(\d+,\d+\)|do\(\)|don't\(\))/).flatten.map do |fn|
+  case fn
+  when "do()"    then "start()"
+  when "don't()" then "stop()"
+  else                fn
   end
-  next if skip
-
-  sum += eval(instruction)
 end
 
-puts sum
+@skip = false
+sum = 0
+
+def mul(a, b) = @skip ? 0 : a * b
+def start     = (@skip = false; 0)
+def stop      = (@skip = true; 0)
+
+sum += eval(instructions.shift) until instructions.empty?
+puts sum # 77_055_967
